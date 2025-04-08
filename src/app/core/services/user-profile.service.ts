4a5aa9996e5d0be50f268336799f822d5fb5d912
gencaydo <gdoyurucu@hotmail.com>
2025-04-08 11:25:47 +0300

    Method Changed

--- src/app/core/services/user-profile.service.ts
+++ src/app/core/services/user-profile.service.ts
@@ -23,7 +23,7 @@ export class UserProfileService {
     const user = this.authStateService.getUser();
     const params = { email: user?.userName};
     
-    return this.apiService.get<ApiResponse<UserProfile>>(this.controller, 'GetUserByEmail', params)
+    return this.apiService.post<ApiResponse<UserProfile>>(this.controller, 'GetUserByEmail', params)
       .pipe(
         tap({
           next: (response) => {
